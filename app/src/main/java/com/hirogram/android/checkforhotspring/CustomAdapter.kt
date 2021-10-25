0afc1ce4c1fd43376e5ad940c2723bb8fe6ca7ba
HirotoMori1029 <morihiroto1029@gmail.com>
2021-10-25 22:05:54 +0900

    Adapterクラスを作成

--- app/src/main/java/com/hirogram/android/checkforhotspring/CustomAdapter.kt
+++ app/src/main/java/com/hirogram/android/checkforhotspring/CustomAdapter.kt
@@ -1,16 +1,34 @@
 package com.hirogram.android.checkforhotspring
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class CustomAdapter {
+class CustomAdapter(private val blgList: ArrayList<Belonging>): RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) :RecyclerView.ViewHolder(view) {
-        val id: TextView = view.findViewById(R.id.tvID)
-        val name: TextView = view.findViewById(R.id.tvName)
+        val tvID: TextView = view.findViewById(R.id.tvID)
+        val tvName: TextView = view.findViewById(R.id.tvName)
         val checkBox: CheckBox = view.findViewById(R.id.checkBox)
 
     }
+
+    override fun onCreateViewHolder (viewGroup: ViewGroup, viewType:Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.list, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val belonging = blgList[position]
+
+        holder.tvID.text = belonging.id.toString()
+        holder.tvName.text = belonging.name
+        holder.checkBox.isChecked = belonging.check
+
+    }
+
+    override fun getItemCount() = blgList.size
 }
