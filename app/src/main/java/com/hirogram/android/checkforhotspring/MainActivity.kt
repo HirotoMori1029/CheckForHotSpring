3766e4ee5f5e93a43c8e76b1fd3634af21d4c806
HirotoMori1029 <morihiroto1029@gmail.com>
2021-12-14 23:28:50 +0900

    Swipeの処理をonSwipedまで記入

--- app/src/main/java/com/hirogram/android/checkforhotspring/MainActivity.kt
+++ app/src/main/java/com/hirogram/android/checkforhotspring/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.hirogram.android.checkforhotspring
 
 import android.content.Intent
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,14 +11,20 @@ import android.view.Menu
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.behavior.SwipeDismissBehavior
 import io.paperdb.Paper
 
 class MainActivity : AppCompatActivity() {
 
-    private var cAdapter: CustomAdapter? = null
+    private lateinit var cAdapter: CustomAdapter
     private var bList = mutableListOf<Belonging>()
+    private val swipeToDismissTouchHelper by lazy {
+        getSwipeToDismissTouchHelper(cAdapter)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +38,9 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = layoutManager
         bList = generateItemList()
         cAdapter = CustomAdapter(bList)
+
+        //touchでの処理するための記述
+        swipeToDismissTouchHelper
         recyclerView.adapter = cAdapter
 
         //StartButtonを押下されたときの処理(暫定)
@@ -48,16 +60,16 @@ class MainActivity : AppCompatActivity() {
         btnDelete.setOnClickListener {
             //todo あとでチェックしていないアイテムを削除するように変更する
             Paper.book().destroy()
-            cAdapter?.bList?.clear()
-            cAdapter?.notifyDataSetChanged()
+            cAdapter.bList?.clear()
+            cAdapter.notifyDataSetChanged()
         }
     }
 
     override fun onResume() {
         super.onResume()
 
-        cAdapter?.bList = generateItemList()
-        cAdapter?.notifyDataSetChanged()
+        cAdapter.bList = generateItemList()
+        cAdapter.notifyDataSetChanged()
         Log.d("onResume", "onResume has been executed")
 
     }
@@ -74,5 +86,58 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu_options_menu_list, menu)
         return super.onCreateOptionsMenu(menu)
     }
+
+    private fun getSwipeToDismissTouchHelper(adapter: CustomAdapter) =
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.ACTION_STATE_IDLE,
+            ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                //positionを取得する
+                val position = viewHolder.absoluteAdapterPosition
+                adapter.bList.removeAt(position)
+                //todo Paperの処理で削除する必要がある
+                val key = adapter.bList[position].name
+                Paper.book().delete(key)
+            }
+
+            override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                super.onChildDraw(
+                    c,
+                    recyclerView,
+                    viewHolder,
+                    dX,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
+
+                //itemの変数を定義
+                val itemView = viewHolder.itemView
+                val background = ColorDrawable(Color.RED)
+                val deleteIcon = AppCompatResources.getDrawable(
+                    this@MainActivity,
+                    //todo iconを作成する
+                    R.drawable.ic_launcher_foreground
+                )
+            }
+        }
+        )
 
 }
