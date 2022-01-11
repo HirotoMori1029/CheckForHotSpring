e1718dd86d35102333e3cc92d65559fb1fce93b5
HirotoMori1029 <morihiroto1029@gmail.com>
2022-01-11 20:39:48 +0900

    RegisterPlaceActivity.ktのボタン処理を追記

--- app/src/main/java/com/hirogram/android/checkforhotspring/RegisterPlaceActivity.kt
+++ app/src/main/java/com/hirogram/android/checkforhotspring/RegisterPlaceActivity.kt
@@ -3,6 +3,7 @@ package com.hirogram.android.checkforhotspring
 import android.content.Context
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -18,6 +19,7 @@ class RegisterPlaceActivity : AppCompatActivity() {
         val tvPlace = findViewById<TextView>(R.id.tvPlace)
         tvPlace.text = getString(R.string.current_place_desc, text)
 
+        //リスト・アダプターの作成
         val plist: MutableList<String> = mutableListOf("Sasebo", "Fukuoka", "Kitakyushu")
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, plist)
         val lvPlace = findViewById<ListView>(R.id.lvPlace)
@@ -30,6 +32,13 @@ class RegisterPlaceActivity : AppCompatActivity() {
             sp.edit().putString(key, place).apply()
         }
         lvPlace.adapter = adapter
+
+        //ボタンが押されたときの処理
+        val btnOk = findViewById<Button>(R.id.btn_ok)
+        btnOk.setOnClickListener {
+            finish()
+        }
+
 
     }
 }
